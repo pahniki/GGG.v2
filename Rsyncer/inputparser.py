d11ad58811f0369b04baa76a095134f555ef01ff
Pakholchuk Nik <pahniki@gmail.com>
2017-03-31 16:58:53 +0300

    multiple remote machines in one request - feature

--- Rsyncer/inputparser.py
+++ Rsyncer/inputparser.py
@@ -7,9 +7,14 @@ stored in output_dict['host_files']
 Password to remote host located in output_dict['password']
 '''
 import argparse
+from utility_cls import Utility
+
+logger = Utility.rsynclog.logger_init('inputparser')
+
 
 class Inputparser:
     """ First stage parser. Using 'argparse' module to pull out all valid parameters."""
+
     @staticmethod
     def inputparse():
         """ Static method which draws out all valid parameters. Based on 'argparse' module. Returns dict,list """
@@ -20,25 +25,55 @@ class Inputparser:
         unknownkeys = []
         keys = []
         parser.add_argument('-process', action="store_true", default=False)
-        parser.add_argument('-pass', action="store", dest="userpass", type=str)
         parser.add_argument('-e', action="store", dest='connection', type=str)
-        parser.add_argument('files', type=str, help='list of files and dirrs to copy', nargs='*')
-        known, unknown = parser.parse_known_args()
+        known, unknown = parser.parse_known_args(['-Pa','-process','/dir','file','username@remote:/dirdir'])
+        # ['-PavSzqi', '-process', '-pass=noOneLiveForever',
+        #  '-h', '/dir', 'some', 'file',
+        #  '[-pass=1234 root:port@hostname:/not\ dir -pass=123 another:2@hostname:/dir andone@more:/dir -pass=urmama69]']
         # Fill arguments in group
+        unknown = unknown
+
+        unknown, hosts = Inputparser.get_hosts(unknown)
+        unknown, filesgarbage = Inputparser.get_files(unknown)
         unknown = set(unknown)
         for i in unknown:
             if i[1:] in single_param:
-                    keys.append(i)
+                keys.append(i)
             else:
                 unknownkeys.append(i)
-        for i in known.files:
-            filesgarbage.append(i)
+
         if known.connection == 'ssh':
             print(known.connection)
             keys.append('-e ssh')
         elif known.connection == 'rsh':
             keys.append('-e rsh')
+
         if known.process:
             keys.append('-process')
-        output_dict.update({'host_files': filesgarbage, 'keys': keys, 'password': known.userpass})
+        output_dict.update({'host_files': filesgarbage, 'keys': keys, 'hosts': hosts})
+
+        Utility.rsynclog.debug_log(logger, output_dict)
         return output_dict, unknownkeys
+
+    @staticmethod
+    def get_hosts(some_list):
+        if (some_list[-1] == ']'):
+            index = some_list.index('[')
+            host_list = some_list[index + 1:-1]
+            some_list = some_list[:index]
+        else:
+            if(some_list[-1].startswith('-pass=')):
+                host_list = some_list[-2:]
+                some_list.remove(some_list[-1])
+                some_list.remove(some_list[-1])
+            else:
+                host_list = [some_list[-1],]
+                some_list.remove(some_list[-1])
+
+        return some_list, host_list
+
+    @staticmethod
+    def get_files(some_list):
+        unknown = filter(lambda x: x.startswith('-'), some_list)
+        files = filter(lambda x: not x.startswith('-'), some_list)
+        return unknown, files
