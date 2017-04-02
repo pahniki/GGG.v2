0e74e3a260c88c02a3395a588f1e67b8f5160cbe
AlexShpil <alexshpil@gmail.com>
2017-04-02 11:12:56 +0300

    Update utility_cls.py
    Added main help, help for connection type, mulltiple remoute hosts usage help.

--- Rsyncer/utility_cls.py
+++ Rsyncer/utility_cls.py
@@ -1,4 +1,7 @@
-# utility
+'''
+Collection of used utilities
+'''
+
 
 class Utility:
     @staticmethod
@@ -17,33 +20,103 @@ class Utility:
             item.self_print()
 
     @staticmethod
+    def port_to_keys(keys_list, port):
+        """ Add '-p port' to a -e params, if port exist """
+        if (port):
+            index = 0
+            key_str = '-e \'ssh -p {}\''.format(port)
+            for item in Utility.gen(keys_list):
+                if (item.startswith('-e')):
+                    index = keys_list.index(item)
+                    keys_list[index] = key_str
+                    break
+            if (not index):
+                keys_list.append(key_str)
+
+        return keys_list
+
+    @staticmethod
     def rsync_all(data_dict):
         for item in Utility.gen(data_dict['client']):
+            keys = list()
+            keys.extend(data_dict['keys'])
+
             item.pinger()
-            item.rsync_cmd(data_dict['keys'], data_dict['host_files'])
+            keys = Utility.port_to_keys(keys, item.port)
+            item.rsync_cmd(keys, data_dict['host_files'])
 
+            #########################Logger section#########################
 
+    class rsynclog:
+        '''
+            Custom logger to store all rsync wrapper actions.
+        '''
 
-'''
-Custom logger to store all rsync wrapper actions.
-'''
-class customlogger:
+        @staticmethod
+        def logger_init(some_str):
+            ''' Initialise new logger '''
+            import logging
+            logger = logging.getLogger(some_str)
+            logger.setLevel(logging.INFO)
+            logger.setLevel(logging.DEBUG)
+            logger.setLevel(logging.ERROR)
+            handler = logging.FileHandler('rsyncer.log')
+            formatter = logging.Formatter('[%(asctime)s] - %(name)11s - %(levelname)6s : %(message)s',datefmt='%d-%m-%y %H:%M')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+            return logger
 
-    ''' Initialise new logger '''
-    def newlogger(self):
-        import logging
-        self.logger = logging.getLogger('rsyncer_logger')
-        self.logger.setLevel(logging.INFO)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler('rsyncer.log')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        @staticmethod
+        def info_log(logger, infostr):
+            ''' Log info message '''
+            logger.info(infostr)
 
-    ''' Log info message '''
-    def info_log(self, infostr):
-        self.logger.info(infostr)
+        @staticmethod
+        def debug_log(logger, infostr):
+            ''' Log debug message '''
+            logger.debug(infostr)
 
-    ''' Log debug message '''
-    def debug_log(self, infostr):
-        self.logger.debug(infostr)
+        @staticmethod
+        def error_log(logger, infostr):
+            ''' Log debug message '''
+            logger.error(infostr)
+
+    class helper:
+        '''Helper object will raise help messages'''
+        @staticmethod
+        def main_help():
+            print('All available keys:\n'
+                  '-process                    If exists raise -process flag for rsync\n'
+                  '-pass=[Password]            Password for connection to remote host\n'
+                  '-e [connection type]        Connection type ssh or rsh\n'
+                  '-P                          Analog rsync --partial --progress\n'
+                  '-a                          Archive mode\n'
+                  '-v                          Verbose input\n'
+                  '-S                          Parse argument files\n'
+                  '-z                          Compress data stream\n'
+                  '-q                          Quiet input\n'
+
+                  'Single remoute host can be entered without [...] brackets.\n'
+                  'Multiple remoute hosts should be entered as list in [...] brackets.\n '
+                  '\n'
+                  '!!After [ and before ] brackets spaces are  necessary!!\n'
+                  '\n'
+                  'Example: \n'
+                  'rsyncer.py -Pa /dir file1 [ username1@remote1 -pass=123 username2@remote2 username3@remote3 -pass=qwe ]\n'
+                  'Valid separators between username and port are: comma, spot, colon (,.:)\n'
+                  'Examples: rsyncer.py /usr root@host\n'
+                  '          rsyncer.py /usr/wildcard* file3.avi root,22@hostname:/junk')
+
+        @staticmethod
+        def connection_type_help():
+            print('Something goes wrong. Try -e ssh, -e rsh or use help.')
+
+        @staticmethod
+        def multiple_host_help():
+            print('Something goes wrong. Spaces are  necessary after open and before closed brackets.\n'
+                  'For more information use help\n'
+                  'Example: [ username1@remote1 -pass=123 username2@remote2 username3@remote3 -pass=qwe ]')
+
+        @staticmethod
+        def random_help():
+            print('Oops, something goes wrong. Try to use help.')
